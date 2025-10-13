6d75bbf97f2663081364f4469481182698527ac9
vincentbelvedere <vbelvedere@strivehealth.com>
2025-10-13 13:28:58 -0600

    adding descriptions, and creating class

--- Python/db_utils.py
+++ Python/db_utils.py
@@ -3,49 +3,69 @@ from sqlite3 import Error
 from sqlite3 import dbapi2 as sqlite
 import csv
 
-# PLEASE DESCRIBE -
-def create_connection(db_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
+class DatabaseLoader:
 
-    return conn
+    # A class to
 
-# PLEASE DESCRIBE -
-def db_query(conn, query):
-    cur = conn.cursor()
-    cur.execute(query)
-    rows = cur.fetchall()
+    def __init__(self, db_path: str):
+        self.db_path = db_path
+        self.conn = self.create_connection()
 
-    for row in rows:
-        print(row)
-
-    return len(rows)
-
-# PLEASE DESCRIBE -
-def db_getinfo(conn, tbl_name):
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM " + tbl_name)
-    column_name_list = [tuple[0] for tuple in cur.description]
-
-    return column_name_list
-
-# PLEASE DESCRIBE -
-def loadcsv(conn, file_name, tbl_name):
     # PLEASE DESCRIBE -
-    csv_file = open(file_name)
-    csv_reader = csv.DictReader(csv_file)
-    insert_sql = 'INSERT INTO ' + tbl_name + ' (' + ','.join(csv_reader.fieldnames) + ') VALUES (' + ','.join(['?'] * len(csv_reader.fieldnames))+ ')'
-    print(insert_sql)
-    # PLEASE DESCRIBE -
-    values = []
-    for datarow in csv_reader:
-        row_values = []
-        for field in csv_reader.fieldnames:
-            row_values.append(datarow[field])
-        values.append(row_values)
+    # This function creates a connection to the sqlite database by initializing the `conn` variable,
+    # and then connecting to whatever db_file is specified
+    # If the connection fails, the error is printed and `None` is returned
+    def create_connection(db_file):
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+        except Error as e:
+            print(e)
 
-    conn.executemany(insert_sql, values)
-    conn.commit()
+        return conn
+
+    # PLEASE DESCRIBE -
+    # This function runs a basic sql query using an established database connection
+    # The SQL query returns all rows fetched, basically a Select *
+    def db_query(conn, query):
+        cur = conn.cursor()
+        cur.execute(query)
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+        return len(rows)
+
+    # PLEASE DESCRIBE -
+    # This is a query that first selects all from a specific table, and then returns only the column names of that table
+    def db_getinfo(conn, tbl_name):
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM " + tbl_name)
+        column_name_list = [tuple[0] for tuple in cur.description]
+
+        return column_name_list
+
+    # PLEASE DESCRIBE -
+    # This is a function that ultimately loads data from a csv into a given table
+    def loadcsv(conn, file_name, tbl_name):
+        # PLEASE DESCRIBE -
+        # It opens the CSV and reads its header to determine column names, then constructs a SQL INSERT statement.
+        # The insert_sql variable is a command that will later push the csv's data into the tbl_name, the print statement shows us what that command will look like
+        csv_file = open(file_name)
+        csv_reader = csv.DictReader(csv_file)
+        insert_sql = 'INSERT INTO ' + tbl_name + ' (' + ','.join(csv_reader.fieldnames) + ') VALUES (' + ','.join(['?'] * len(csv_reader.fieldnames))+ ')'
+        print(insert_sql)
+        # PLEASE DESCRIBE -
+        # The CSV rows are collected into a list of lists (`values`) for bulk insertion.
+        values = []
+        for datarow in csv_reader:
+            row_values = []
+            for field in csv_reader.fieldnames:
+                row_values.append(datarow[field])
+            values.append(row_values)
+
+        #This section is an execution of the insert_sql command we created earlier, and commits the changes
+
+        conn.executemany(insert_sql, values)
+        conn.commit()
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ibotta-takehome
 Ibotta take home assignment 
+
+Part 1:
+My first steps were to set up a new GitHub repo, as I figured it would be easiest to work in
+I then went over the existing code that was given to me to figure out what gaps I need to fill, and what I will need to write myself.
+This is where I added the descriptions to the existing code.
+
+Now, seeing as we want to incorporate OOP, I added all the functions in the db_utils file into a class, `Database_Loader`
